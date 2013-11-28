e34a764d20a7c35bbace4d3a5132a895a8a8b720
Matt Kline <slavik262@gmail.com>
2013-11-28 02:25:48 -0600

    To Github!

---
+++ README.md
@@ -0,0 +1,14 @@
+# Charge
+
+## What is it?
+
+A joke of a project so I can mess around with [D](http://dlang.org).
+
+## What does it do?
+
+It reads laptop battery status using `acpi` and formats it the way I want.
+I use it with [conky](http://conky.sourceforge.net/).
+
+## Couldn't you have done this with sed, or perl, or something far simpler?
+
+See "What is it?"
---
+++ charge.d
@@ -0,0 +1,16 @@
+import std.exception;
+import std.regex;
+import std.stdio;
+
+void main(string args[])
+{
+	File af;
+	af.popen("acpi");
+	const string acpi = af.readln();
+	auto ctr = ctRegex!(`(Charging|Discharging|Full|Unknown), (\d+%)(?:, (\d+):(\d+):\d+)?`);
+	auto caps = matchAll(acpi, ctr).captures();
+	string lineout = caps[1][0] ~ " " ~ caps[2];
+	if (caps[3].length && caps[4].length)
+		lineout ~= " " ~ caps[3] ~ "h " ~ caps[4] ~ "m";
+	writeln(lineout);
+}
